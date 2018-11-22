9d64862e934eba8ae44ad100d3c8789782aa0ddb
k-kozaki <k-kozaki@froide.co.jp>
2018-11-22 10:32:58 +0900

    ライセンス登録フォーム作成

---
+++ app/controllers/licenses_controller.rb
@@ -0,0 +1,17 @@
+class LicensesController < ApplicationController
+  def index; end
+
+  def create
+    # @download = Download.new(download_params)
+    #
+    # respond_to do |format|
+    #   if @download.save
+    #     format.html { redirect_to @download, notice: 'Download was successfully created.' }
+    #     format.json { render :show, status: :created, location: @download }
+    #   else
+    #     format.html { render :new }
+    #     format.json { render json: @download.errors, status: :unprocessable_entity }
+    #   end
+    # end
+  end
+end
