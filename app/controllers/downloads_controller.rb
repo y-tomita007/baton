7569602318786da54287f7888b8d8fa26e5f27c6
k-kozaki <k-kozaki@froide.co.jp>
2018-11-26 15:07:30 +0900

    [ダウンロード画面]ダウンロード完了画面追加

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -6,5 +6,6 @@ class DownloadsController < ApplicationController
     filepath = Rails.root.join('public', 'download_test.txt')
     stat = File.stat(filepath)
     send_file(filepath, filename: 'download_test.txt', length: stat.size)
+    render
   end
 end
