cd292cd7c6cdb7aacc79a4d4de63a6c8ad37c3bc
k-kozaki <k-kozaki@froide.co.jp>
2018-11-26 11:10:27 +0900

    [ダウンロード画面]仮ダウンロード機能追加

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -1,3 +1,10 @@
 class DownloadsController < ApplicationController
   def index; end
+
+  # REVIEW: DownloadsControllerにdownloadアクションがあるのが気持ち悪い
+  def download
+    filepath = Rails.root.join('public', 'download_test.txt')
+    stat = File.stat(filepath)
+    send_file(filepath, filename: 'download_test.txt', length: stat.size)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :licenses, only: %i[index create]
-  resources :downloads, only: :index
+  resources :downloads, only: :index do
+    collection do
+      get :download
+    end
+  end
 end
